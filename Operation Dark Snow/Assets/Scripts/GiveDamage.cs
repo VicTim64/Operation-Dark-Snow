b3fc6caecced5f5566a0e0688d19eed8cba7b182
Vlad Olareanu <vlad.olareanu@gmail.com>
2019-04-05 16:30:10 +0100

    AI CW

---
+++ Operation Dark Snow/Assets/Scripts/GiveDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveDamage : MonoBehaviour {
+	public bool isDamaging;
+	public float damage = 5;
+
+	private void OnTriggerStay(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			col.SendMessage ((isDamaging) ? "TakeDamage" : "HealDamage", Time.deltaTime * damage);
+		}
+	
+	}}
